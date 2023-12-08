60e6687f39249f18787ddf3acc65fd76fae5518d
Andre <Andre@MacBook-Air-de-Andre.local>
2023-12-08 12:00:38 -0300

    Simple drop in replacement of numpy by cupy

--- exotic/api/ew.py
+++ exotic/api/ew.py
@@ -58,7 +58,7 @@ class ExoplanetWatch():
 
         # download the results JSON
         #r = urllib.request.urlopen(self.url)
-        r = urllib.request.urlopen(urllib.request.Request(self.url, data=None, headers={'User-Agend': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}))
+        r = urllib.request.urlopen(urllib.request.Request(self.url, data=None, headers={'User-agent': 'Mozilla/5.0'}))
         jdata = json.loads(r.read().decode(r.info().get_param('charset') or 'utf-8'))
         self.alldata = jdata['items']
 
