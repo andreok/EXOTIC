4d528dcecea2835dd48d74adcf9111f604582b0d
Andre <Andre@MacBook-Air-de-Andre.local>
2024-01-10 19:14:52 -0300

    Simple drop in replacement of numpy by cupy

--- exotic/api/elca.py
+++ exotic/api/elca.py
@@ -858,7 +858,16 @@ class glc_fitter(lc_fitter):
 
         noop = lambda *args, **kwargs: None
         if self.verbose:
-            self.results = ReactiveNestedSampler(freekeys, loglike, prior_transform).run(max_ncalls=1e6, show_status=True)
+            #self.results = ReactiveNestedSampler(freekeys, loglike, prior_transform).run(max_ncalls=1e6, show_status=True)
+            #self.results = ReactiveNestedSampler(freekeys, loglike, prior_transform).run(max_ncalls=2e6) # patched
+            #print(type(freekeys))
+            #print(type(loglike))
+            #print(type(prior_transform))
+            sampler = ReactiveNestedSampler(freekeys, loglike, prior_transform)
+            nsteps = 2 * len(freekeys)
+            #sampler.stepsampler = ultranest.stepsampler.SliceSampler(nsteps=nsteps,generate_direction=ultranest.stepsampler.generate_mixture_random_direction)
+            sampler.stepsampler = ultranest.stepsampler.SliceSampler(nsteps=nsteps,generate_direction=ultranest.stepsampler.generate_cube_oriented_direction)
+            self.results = sampler.run(max_ncalls=2e6) # pached
         else:
             self.results = ReactiveNestedSampler(freekeys, loglike, prior_transform).run(max_ncalls=1e6, show_status=False, viz_callback=noop)
 
