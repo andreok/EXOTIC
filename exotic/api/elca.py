adb24baebd085c6f27af1d2d5753d16614378fcd
Andre <Andre@MacBook-Air-de-Andre.local>
2024-01-13 16:33:31 -0300

    Simple drop in replacement of numpy by cupy

--- exotic/api/elca.py
+++ exotic/api/elca.py
@@ -111,8 +111,8 @@ def transit(times, values):
                       values['ecc'], values['inc'], values['omega'],
                       values['tmid'], times, precision=3) #pylightcurve-torch has a different syntax 
         return model.cpu().numpy() # must convert from PyTorch GPU arrays to Numpy arrays for CPU
-    except AttributeError:
-    #except TypeError:
+    #except AttributeError:
+    except TypeError:
         model = pytransit([values['u0'], values['u1'], values['u2'], values['u3']],
                       values['rprs'], values['per'], values['ars'],
                       values['ecc'], values['inc'], values['omega'],
