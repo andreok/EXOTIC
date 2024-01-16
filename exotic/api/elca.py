80d4d03b0e897b49cddadecf7f2da47c7365e4d8
Andre <Andre@MacBook-Air-de-Andre.local>
2024-01-16 16:28:35 -0300

    Simple drop in replacement of numpy by cupy

--- exotic/api/elca.py
+++ exotic/api/elca.py
@@ -106,9 +106,8 @@ def transit(times, values):
                       torch.tensor([[values['rprs']]], device=cuda), torch.tensor([[values['per']]], device=cuda), 
                       torch.tensor([[values['ars']]], device=cuda), torch.tensor([[values['ecc']]], device=cuda), 
                       torch.tensor([[values['inc']]], device=cuda), torch.tensor([[values['omega']]], device=cuda),
-                      torch.tensor([[values['tmid']]], device=cuda), torch.from_numpy(times).float().cuda(), precision=3, n_pars=1) #pylightcurve-torch has a different syntax, and requires PyTorch Tensors instead of Nympy arrays
-        #return model.cpu().numpy() # must convert from PyTorch GPU Tensors to Numpy arrays for CPU
-        return model.numpy() # must convert from PyTorch GPU Tensors to Numpy arrays
+                      torch.tensor([[values['tmid']]], device=cuda), torch.from_numpy(times).float().cuda(), precision=3, n_pars=values['tmid'].size) #pylightcurve-torch has a different syntax, and requires PyTorch Tensors instead of Nympy arrays
+        return model.cpu().numpy() # must convert from PyTorch GPU Tensors to Numpy arrays for CPU
     #except AttributeError:
     #except TypeError:
     except NameError:
