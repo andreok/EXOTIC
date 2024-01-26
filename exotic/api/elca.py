1b413fba6e88d066e534e933fbd84736cd131185
Andre <Andre@MacBook-Air-de-Andre.local>
2024-01-26 19:17:26 -0300

    Simple drop in replacement of numpy by cupy

--- exotic/api/elca.py
+++ exotic/api/elca.py
@@ -1400,7 +1400,7 @@ class glc_fitter(lc_fitter):
             #sampler.stepsampler = ultranest.stepsampler.SliceSampler(nsteps=nsteps,generate_direction=ultranest.stepsampler.generate_cube_oriented_direction)
             try:
                 #sampler.stepsampler = ultranest.popstepsampler.PopulationRandomWalkSampler(popsize=40,nsteps=nsteps,generate_direction=ultranest.popstepsampler.generate_region_random_direction)
-                sampler.stepsampler = ultranest.popstepsampler.PopulationSliceSampler(popsize=40,nsteps=nsteps,generate_direction=ultranest.popstepsampler.generate_cube_oriented_direction)
+                sampler.stepsampler = ultranest.popstepsampler.PopulationSliceSampler(popsize=80,nsteps=nsteps,generate_direction=ultranest.popstepsampler.generate_cube_oriented_direction)
                 self.results = np.asnumpy(np.from_dlpack(sampler.run(max_ncalls=2e6, show_status=True))) # pached
             except AttributeError:
                 #sampler.stepsampler = ultranest.stepsampler.SliceSampler(nsteps=nsteps,generate_direction=ultranest.stepsampler.generate_mixture_random_direction)
