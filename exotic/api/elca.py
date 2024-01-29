b37e25ea781dc0146dd61980703e15072724a1cb
Andre <Andre@MacBook-Air-de-Andre.local>
2024-01-29 19:12:08 -0300

    Simple drop in replacement of numpy by cupy

--- exotic/api/elca.py
+++ exotic/api/elca.py
@@ -214,7 +214,7 @@ def planet_orbit(period, sma_over_rs, eccentricity, inclination, periastron, mid
 
     return [x, y, z]
 
-@maybe_decorate(jit(nopython=True, parallel=True, cache=True))
+#@maybe_decorate(jit(nopython=True, parallel=True, cache=True))
 def integral_r_claret(limb_darkening_coefficients, r):
     # please see original: https://github.com/ucl-exoplanets/pylightcurve/blob/master/pylightcurve/models/exoplanet_lc.py
     a1, a2, a3, a4 = limb_darkening_coefficients
