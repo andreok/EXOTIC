d9a7ce11b6ff746db6cc2c7402876f2cfc239a0d
Andre <Andre@MacBook-Air-de-Andre.local>
2024-02-01 19:14:32 -0300

    Simple drop in replacement of numpy by cupy

--- exotic/api/elca.py
+++ exotic/api/elca.py
@@ -139,10 +139,10 @@ def planet_orbit(period, sma_over_rs, eccentricity, inclination, periastron, mid
         case_circular = (eccentricity == 0) * (ww == 0)
         case_not_circular = (eccentricity != 0) + (ww != 0)
 
-        aa = jnp.select(case_periastron, jnp.full_like(periastron, 1.0, dtype=jnp.float64) * jnp.pi / 2 - periastron, jnp.full_like(periastron, 5.0, dtype=jnp.float64) * jnp.pi / 2 - periastron)
-        bb = jnp.select(case_circular, sma_over_rs * jnp.cos(2 * jnp.pi * (time_array - mid_time) / period), 2 * jnp.arctan(jnp.sqrt((1 - eccentricity) / (1 + eccentricity)) * jnp.tan(aa / 2)))
+        aa = jax.lax.select(case_periastron, jnp.full_like(periastron, 1.0, dtype=jnp.float64) * jnp.pi / 2 - periastron, jnp.full_like(periastron, 5.0, dtype=jnp.float64) * jnp.pi / 2 - periastron)
+        bb = jax.lax.select(case_circular, sma_over_rs * jnp.cos(2 * jnp.pi * (time_array - mid_time) / period), 2 * jnp.arctan(jnp.sqrt((1 - eccentricity) / (1 + eccentricity)) * jnp.tan(aa / 2)))
 
-        bb = jnp.select(case_not_circular * (bb < 0), bb + 2 * jnp.pi, bb)
+        bb = jax.lax.select(case_not_circular * (bb < 0), bb + 2 * jnp.pi, bb)
 
         mid_time = mid_time.astype(jnp.float64) - (period / 2.0 / jnp.pi) * (bb - eccentricity * jnp.sin(bb))
         m = (time_array - mid_time - jnp.int_((time_array - mid_time) / period) * period) * 2.0 * jnp.pi / period
@@ -161,18 +161,18 @@ def planet_orbit(period, sma_over_rs, eccentricity, inclination, periastron, mid
             ii += 1
             jax.lax.cond(ii < 10000, lambda _: None, lambda ii: jax.debug.callback(raise_error, ii), ii) # setting a limit of 1k iterations - arbitrary limit
             return (u0, u1, ii)
-        u0, u1, ii = jnp.select(case_circular, tuple([u0, u1, ii]), jax.lax.while_loop(cond_fun=cond, body_fun=body, init_val=tuple([u0, u1, ii])))
+        u0, u1, ii = jax.lax.select(case_circular, tuple([u0, u1, ii]), jax.lax.while_loop(cond_fun=cond, body_fun=body, init_val=tuple([u0, u1, ii])))
         
-        vv = jnp.select(case_circular, 2 * jnp.pi * (time_array - mid_time) / period, 2 * jnp.arctan(jnp.sqrt((1 + eccentricity) / (1 - eccentricity)) * jnp.tan((u1) / 2)))
+        vv = jax.lax.select(case_circular, 2 * jnp.pi * (time_array - mid_time) / period, 2 * jnp.arctan(jnp.sqrt((1 + eccentricity) / (1 - eccentricity)) * jnp.tan((u1) / 2)))
         
-        rr = jnp.select(case_circular, 0., sma_over_rs * (1 - (eccentricity ** 2)) / (jnp.ones_like(vv) + eccentricity * jnp.cos(vv)))
+        rr = jax.lax.select(case_circular, 0., sma_over_rs * (1 - (eccentricity ** 2)) / (jnp.ones_like(vv) + eccentricity * jnp.cos(vv)))
 
-        aa = jnp.select(case_circular, 0., jnp.cos(vv + periastron))
-        bb = jnp.select(case_circular, bb, jnp.sin(vv + periastron))
+        aa = jax.lax.select(case_circular, 0., jnp.cos(vv + periastron))
+        bb = jax.lax.select(case_circular, bb, jnp.sin(vv + periastron))
 
-        x = jnp.select(case_circular, bb * jnp.sin(inclination), rr * bb * jnp.sin(inclination))
-        y = jnp.select(case_circular, sma_over_rs * jnp.sin(vv), rr * (-aa * jnp.cos(ww) + bb * jnp.sin(ww) * jnp.cos(inclination)))
-        z = jnp.select(case_circular, - bb * jnp.cos(inclination), rr * (-aa * jnp.sin(ww) - bb * jnp.cos(ww) * jnp.cos(inclination)))
+        x = jax.lax.select(case_circular, bb * jnp.sin(inclination), rr * bb * jnp.sin(inclination))
+        y = jax.lax.select(case_circular, sma_over_rs * jnp.sin(vv), rr * (-aa * jnp.cos(ww) + bb * jnp.sin(ww) * jnp.cos(inclination)))
+        z = jax.lax.select(case_circular, - bb * jnp.cos(inclination), rr * (-aa * jnp.sin(ww) - bb * jnp.cos(ww) * jnp.cos(inclination)))
     except NameError:
         inclination = inclination * np.pi / 180.0
         periastron = periastron * np.pi / 180.0
